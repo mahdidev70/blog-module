1d9c588b7471aa20687ad9a2af1eae0007dcc35b
next <mahdidev70@gmail.com>
2023-09-30 16:17:36 +0330

    fix: test deployment

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,7 @@
 
 
 
-// Route::get('/ffff', [ModuleHomeController::class, 'index']);
 
-Route::get('/ddd', function () {
-
-    return 'sdfdddd';
-});
+// Route::get('/ddd', function () {
+//     return 'sdfdddd';
+// });
