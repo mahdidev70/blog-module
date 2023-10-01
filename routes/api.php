3d3c54305d922c04f5c729ee37aa1561d66712c8
AmirMahdi-for <amirmahdifor@gmail.com>
2023-10-01 11:19:02 +0330

    up api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
+use TechStudio\Blog\app\Http\Controllers\ArticleController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
 
+Route::prefix('articles')->group(function () {
 
-// Route::get('/ddd', function () {
-//     return 'sdfdddd';
-// });
+    Route::get('/archive/common', [ArticleController::class, 'articlesArchiveCommon']);
+    Route::get('/archive/list', [ArticleController::class, 'listArticles']);
+
+});
