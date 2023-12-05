ffe3907ee1763f21cf0353195b2bc3c9942b26f1
Amirmahdi-for <amirmahdifor@gmail.com>
2023-12-05 14:40:54 +0330

    up api routes

--- routes/api.php
+++ routes/api.php
@@ -22,71 +22,76 @@ use Illuminate\Support\Facades\Config;
 */
 
     // ============ CLIENT SIDE ===============
-    // Route::prefix('articles')->group(function () {
+    Route::prefix('articles')->group(function () {
 
-    //     Route::get('/archive/common', [ArticleController::class, 'articlesArchiveCommon']); //=> Done
-    //     Route::get('/archive/list', [ArticleController::class, 'listArticles']); //=> Done
-    //     Route::get('/category/{slug}/common', [ArticleController::class, 'articlesByCategoryCommon']); //=> Done
+        Route::get('/archive/common', [ArticleController::class, 'articlesArchiveCommon']); //=> Done
+        Route::get('/archive/list', [ArticleController::class, 'listArticles']); //=> Done
+        Route::get('/category/{slug}/common', [ArticleController::class, 'articlesByCategoryCommon']); //=> Done
         
-    // });
+    });
 
-    // Route::prefix('home')->group(function() {
+    Route::prefix('home')->group(function() {
         
-    //     Route::get('/common', [BlogHomeController::class, 'getHomeCommon']); //=> Done
-    //     Route::get('/data', [BlogHomeController::class, 'getHomeData']); //=> Done
+        Route::get('/common', [BlogHomeController::class, 'getHomeCommon']); //=> Done
+        Route::get('/data', [BlogHomeController::class, 'getHomeData']); //=> Done
         
-    // });
+    });
 
     
-    // Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware('auth:sanctum')->group(function () {
 
-    //     Route::prefix('article')->group(function() {
+        Route::prefix('article')->group(function() {
             
-    //         Route::get('/{slug}/comments', [CommentController::class, 'getComments']); //=> Done
-    //         Route::get('/{slug}', [ArticleController::class, 'getArticle']); //=> Done
+            Route::get('/{slug}/comments', [CommentController::class, 'getComments']); //=> Done
+            Route::get('/{slug}', [ArticleController::class, 'getArticle']); //=> Done
     
-    //     });
+        });
         
-    //     Route::prefix('article/{slug}')->group(function() {
+        Route::prefix('article/{slug}')->group(function() {
             
-    //         Route::post('/bookmark', [ArticleController::class, 'storeBookmark']); //=> Done
-    //         Route::post('/feedback', [ArticleController::class, 'storeFeedback']); //=> Done
-    //         Route::post('/comments', [CommentController::class, 'store']); //=> Done
-            Route::post('/comments/{id}/feedback', [CommentController::class, 'storeFeedback']);
+            Route::post('/bookmark', [ArticleController::class, 'storeBookmark']); //=> Done
+            Route::post('/feedback', [ArticleController::class, 'storeFeedback']); //=> Done
+            Route::post('/comments', [CommentController::class, 'store']); //=> Done
+            Route::post('/comments/{id}/feedback', [CommentController::class, 'storeFeedback']); //=> Done
                     
-    //     });
+        });
         
-        // // ============= PANEL SIDE ===============
-        // Route::get('/article_editor/common', [ArticleController::class, 'getEditorCommon']); // Done
-        // Route::get('/article_editor/data/{id}', [ArticleController::class, 'getEditorData']); // Done
-        Route::put('/article_editor/data/', [ArticleController::class, 'updateEditorData']); // Done
+        // ============= PANEL SIDE ===============
+        
+        Route::prefix('article_editor')->group(function (){
 
-        // Route::prefix('panel')->group(function () {
+            Route::get('/common', [ArticleController::class, 'getEditorCommon']); //=> Done
+            Route::get('/data/{id}', [ArticleController::class, 'getEditorData']); //=> Done
+            Route::put('/data', [ArticleController::class, 'updateEditorData']); //=> Done
+
+        });
+
+        Route::prefix('panel')->group(function () {
         
-        //     // ---- article ----
-        //     Route::get('/data', [ArticleController::class, 'getArticleListData']); // Done
-        //     Route::get('/articles/common', [ArticleController::class, 'getArticleListCommon']); // Done
-        //     Route::put('/articles/set_status', [ArticleController::class, 'updateArticlesStatus']); // Done
-        //     Route::post('/articles/upload_cover', [ArticleController::class, 'uploadArticleCover']);
-        //     Route::post('/articles/inline_media', [ArticleController::class, 'uploadArticleContent']);
+            // ---- article ----
+            Route::get('/data', [ArticleController::class, 'getArticleListData']); //=> Done
+            Route::get('/articles/common', [ArticleController::class, 'getArticleListCommon']); //=> Done
+            Route::put('/articles/set_status', [ArticleController::class, 'updateArticlesStatus']); //=> Done
+            Route::post('/articles/upload_cover', [ArticleController::class, 'uploadArticleCover']);
+            Route::post('/articles/inline_media', [ArticleController::class, 'uploadArticleContent']);
             
-        //     // ---- category: ----
-        //     Route::get('/category/common', [CategoriesController::class, 'getCommonListCategory']); // Done
-        //     Route::get('/categories/list', [CategoriesController::class, 'listCategory']); // Done
-        //     Route::put('/categories/update', [CategoriesController::class, 'createUpdateCategory']); // Done
-        //     Route::put('/categories/set_status', [CategoriesController::class, 'updateCategoryStatus']); // Done
+            // ---- category ----
+            Route::get('/category/common', [CategoriesController::class, 'getCommonListCategory']); //=> Done
+            Route::get('/categories/list', [CategoriesController::class, 'listCategory']); //=> Done
+            Route::put('/categories/update', [CategoriesController::class, 'createUpdateCategory']); //=> Done
+            Route::put('/categories/set_status', [CategoriesController::class, 'updateCategoryStatus']); //=> Done
         
-        //     // ---- tag: ----
-        //     Route::get('/tag/common', [TagController::class, 'getCommonListTag']); // Done
-        //     Route::get('/tags/list', [TagController::class, 'listTags']); // Done
-        //     Route::put('/tags/update', [TagController::class, 'createUpdateTags']); // Done
-        //     Route::put('/tags/set_status', [TagController::class, 'updateTagsStatus']); // Done
+            // ---- tag ----
+            Route::get('/tag/common', [TagController::class, 'getCommonListTag']); //=> Done
+            Route::get('/tags/list', [TagController::class, 'listTags']); //=> Done
+            Route::put('/tags/update', [TagController::class, 'createUpdateTags']); //=> Done
+            Route::put('/tags/set_status', [TagController::class, 'updateTagsStatus']); //=> Done
         
-        //     // ---- comment: ----
-        //     Route::get('/articles/comments/data', [CommentController::class, 'getArticleCommentsListData']); // Done
-        //     Route::get('/articles/comments/common', [CommentController::class, 'getArticleCommentsListCommon']); // Done
-        //     Route::put('/articles/comments/update', [CommentController::class, 'updateArticleCommentsStatus']); // Done
-        //     Route::put('/articles/comments/{comment_id}',[CommentController::class, 'editArticleCommentText']); // Done
+            // ---- comment ----
+            Route::get('/articles/comments/data', [CommentController::class, 'getArticleCommentsListData']); //=> Done
+            Route::get('/articles/comments/common', [CommentController::class, 'getArticleCommentsListCommon']); //=> Done
+            Route::put('/articles/comments/update', [CommentController::class, 'updateArticleCommentsStatus']); //=> Done
+            Route::put('/articles/comments/{comment_id}',[CommentController::class, 'editArticleCommentText']); //=> Done
         
-        // });
-    // });
+        });
+    });
