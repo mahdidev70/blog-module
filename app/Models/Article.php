c8b8697eff676cd86152ef78d7d3f232d18c9262
next <mahdidev70@gmail.com>
2023-12-31 11:00:03 +0330

    fix: use user_id as foreignkey

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -62,13 +62,13 @@ class Article extends Model
 
     public function author()
     {
-        return $this->belongsTo(UserProfile::class);
+        return $this->belongsTo(UserProfile::class, 'user_id');
     }
 
     // // TODO check (remove?)
     public function user()
     {
-        return $this->belongsTo(UserProfile::class);
+        return $this->belongsTo(UserProfile::class, 'user_id');
     }
     public function category()
     {
