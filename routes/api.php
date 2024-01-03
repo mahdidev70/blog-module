ee6ac209150de03f776450c4f60885dc3abfceae
behzadi <fateme.behzadi@diginext.ir>
2024-01-03 13:10:10 +0330

    add permission to roles

--- routes/api.php
+++ routes/api.php
@@ -33,34 +33,34 @@ use Illuminate\Support\Facades\Config;
     });
 
     Route::prefix('home')->group(function() {
-        
+
         Route::get('/common', [BlogHomeController::class, 'getHomeCommon']); //=> Done
         Route::get('/data', [BlogHomeController::class, 'getHomeData']); //=> Done
-        
+
     });
 
     Route::prefix('article')->group(function() {
-        
+
         Route::get('/{slug}/comments', [CommentController::class, 'getComments']); //=> Done
         Route::get('/{slug}', [ArticleController::class, 'getArticle']); //=> Done
 
     });
-    
+
     Route::middleware('auth:sanctum')->group(function () {
 
-        
+
         Route::prefix('article/{slug}')->group(function() {
-            
+
             Route::post('/bookmark', [ArticleController::class, 'storeBookmark']); //=> Done
             Route::post('/feedback', [ArticleController::class, 'storeFeedback']); //=> Done
             Route::post('/comments', [CommentController::class, 'store']); //=> Done
             Route::post('/comments/{id}/feedback', [CommentController::class, 'storeFeedback']); //=> Done
-                    
+
         });
-        
+
         // ============= PANEL SIDE ===============
-        
-        Route::prefix('article_editor')->group(function (){
+
+        Route::prefix('article_editor')->middleware('can:blogs')->group(function (){
 
             Route::get('/common', [ArticleController::class, 'getEditorCommon']); //=> Done
             Route::get('/data/{id}', [ArticleController::class, 'getEditorData']); //=> Done
@@ -68,11 +68,11 @@ use Illuminate\Support\Facades\Config;
 
         });
 
-        Route::prefix('panel')->group(function () {
-        
+        Route::prefix('panel')->middleware('can:blogs')->group(function () {
+
             // ---- article ----
             Route::prefix('/articles')->group(function () {
-                
+
                 Route::get('/data', [ArticleController::class, 'getArticleListData']); //=> Done
                 Route::get('/common', [ArticleController::class, 'getArticleListCommon']); //=> Done
                 Route::put('/set_status', [ArticleController::class, 'updateArticlesStatus']); //=> Done
@@ -80,24 +80,24 @@ use Illuminate\Support\Facades\Config;
                 Route::post('/inline_media', [ArticleController::class, 'uploadArticleContent']);
 
             });
-            
+
             // ---- category ----
             Route::get('/category/common', [CategoriesController::class, 'getCommonListCategory']); //=> Done
             Route::get('/categories/list', [CategoriesController::class, 'listCategory']); //=> Done
             Route::put('/categories/update', [CategoriesController::class, 'createUpdateCategory']); //=> Done
             Route::put('/categories/set_status', [CategoriesController::class, 'updateCategoryStatus']); //=> Done
-        
+
             // ---- tag ----
             Route::get('/tag/common', [TagController::class, 'getCommonListTag']); //=> Done
             Route::get('/tags/list', [TagController::class, 'listTags']); //=> Done
             Route::put('/tags/update', [TagController::class, 'createUpdateTags']); //=> Done
             Route::put('/tags/set_status', [TagController::class, 'updateTagsStatus']); //=> Done
-        
+
             // ---- comment ----
             Route::get('/articles/comments/data', [CommentController::class, 'getArticleCommentsListData']); //=> Done
             Route::get('/articles/comments/common', [CommentController::class, 'getArticleCommentsListCommon']); //=> Done
             Route::put('/articles/comments/update', [CommentController::class, 'updateArticleCommentsStatus']); //=> Done
             Route::put('/articles/comments/{comment_id}',[CommentController::class, 'editArticleCommentText']); //=> Done
-        
+
         });
     });
