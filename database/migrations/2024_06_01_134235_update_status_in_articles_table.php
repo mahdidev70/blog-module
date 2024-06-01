9b43aba1a04d7ec525a1d39b2f9e3b708e4d8a3c
Reyhaneh <reyhaneh.alihosseini@gmail.com>
2024-06-01 09:31:39 +0330

    feat: add ready_to_publish status to blog articles

---
+++ database/migrations/2024_06_01_134235_update_status_in_articles_table.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TechStudio\Blog\database\Migrations;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * This table stores blog articles.
+ *
+ * - "content": This field contains a JSON array of *blocks* that are supported by a page builder.
+ */
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('blog_articles', function (Blueprint $table) {
+            $table->enum('status', ['published', 'draft', 'ready_to_publish', 'hidden', 'deleted'])->default('published')->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('blog_articles');
+    }
+};
