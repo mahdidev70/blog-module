872a10fac8877a22440d1cb390e92bc769e55be5
Reyhaneh <reyhaneh.alihosseini@gmail.com>
2024-06-22 15:06:51 +0330

    fix: edit article permission form request

--- app/Http/Requests/Article/UpdateRequest.php
+++ app/Http/Requests/Article/UpdateRequest.php
@@ -3,10 +3,26 @@
 namespace TechStudio\Blog\app\Http\Requests\Article;
 
 use Illuminate\Foundation\Http\FormRequest;
+use TechStudio\Blog\app\Models\Article;
 use TechStudio\Blog\app\Rules\FilePrefix;
 
 class UpdateRequest extends FormRequest
 {
+    public function authorize(): bool
+    {
+        if ($this->id) {
+            $article = Article::query()->where('id', $this->id)->where('language', $this->route('locale'))->firstOrFail();
+
+            $user = auth()->user();
+
+            if (! $user->can('blogs')) {
+                return $article->author_id == $user->id;
+            }
+        }
+
+        return true;
+    }
+
     public function rules(): array
     {
         return [
