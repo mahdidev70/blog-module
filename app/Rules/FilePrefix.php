594993db76fce0ad9d0c108935c9776da051dc08
Reyhaneh <reyhaneh.alihosseini@gmail.com>
2024-06-22 15:35:49 +0330

    fix: edit article permission form request

--- app/Rules/FilePrefix.php
+++ app/Rules/FilePrefix.php
@@ -14,7 +14,7 @@ class FilePrefix implements Rule
      *
      * @return bool
      */
-    public function passes($attribute, $value): bool
+    public function passes($attribute, $value)
     {
         return str_starts_with($value, env('FILE_PREFIX', 'https://storage-demo-seller-hub.digikala.com'));
     }
